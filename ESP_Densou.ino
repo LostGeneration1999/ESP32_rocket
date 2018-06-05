69f8c39316c5f454eb386e9849567349b75d4a96
LOST_GENERATION <masaya4426saganosen@gmail.com>
2018-06-05 22:06:59 +0900

    Update ESP_Densou.ino

--- ESP_Densou.ino
+++ ESP_Densou.ino
@@ -5,7 +5,6 @@
 #include <Servo.h>
 #include <SD.h>
 #ifdef _ESP32_HAL_I2C_H_
-
 // デバイスのI2Cアドレス(Default モード)
 #define SENSOR_ADRS  0x48
 #define SDA_PIN 21
@@ -17,7 +16,6 @@ const int motorPin = 14;
 const int flightPin = 26;
 // CSを5に設定
 const uint8_t cs_SD = 5; 
-// SDに格納されるファイル
 const char* fname = "/ESP_Densou_test.txt";
 
 //モーター固有の静止値
@@ -30,6 +28,12 @@ static unsigned long time_m;
 boolean flightState = HIGH; 
 //フライトピンが外されからプログラムの実行回数
 int i = 0;
+//加速度センサーが負の値を取り始めてからのカウント
+int n = 0;
+//ms以上でタイマー発動
+const int open_time = 8000;
+//パラシュート解放の限界時間
+const int limit_time = 15000;
 int ans;
 
 // 温度センサーライブラリの生成
@@ -42,11 +46,17 @@ Servo myservo;
 
 //モーターの動作に関する関数
 void flight_motor(){
- const int pos = motor_kinetic;
- Serial.println(" angle:  ");
- Serial.println(pos);
- myservo.write(pos);
+// Serial.println(" angle:  ");
+// Serial.println("     ");
+// Serial.println(pos);
+// Serial.println("     ");
+ myservo.write(motor_kinetic);
  //割り込み終了後loop関数に戻る
+}
+
+void motor_unactive(){
+  Serial.println("stop...");
+  myservo.write(motor_static);
 }
 
 //MPU9250の設定に関する関数
@@ -75,16 +85,16 @@ void BMP_setup(){
   //BMP180センサーとの通信準備
 //後に変更するコード→最終的にはエラーが出ても動作するように
   if (!bmp.begin()) {
-  Serial.println("BMP180 connection NG");
+//  Serial.println("BMP180 connection NG");
   }
-  Serial.println("BMP180 connection OK");
+//  Serial.println("BMP180 connection OK");
 }
 
 //ADT71
 /*
 void ADT_setup(){
   // 温度センサーの初期化を行う(16bitの解像度 動作モードはシャットダウン)
- ans = Temp.Begin() ;
+  ans = Temp.Begin() ;
   if (ans == 0) Serial.println("Initialization normal") ;
   else {
      Serial.print("Initialization abnormal ans=") ;
@@ -96,7 +106,7 @@ void ADT_setup(){
 */
 
 //データを収集、SDに格納に関する関数
-void data(){
+float data(){
 //変数を定義
 float temperature,pressure,alti,temp;
 float ax,ay,az,gx,gy,gz,mx,my,mz,a_sqrt,m_hori;
@@ -110,8 +120,7 @@ if (ans == 0) {
           Serial.write(0xDF);
           Serial.println("C");
  } else Serial.println("NG");
- */
-
+*/
 //データを変数に代入
 
 ax = mySensor.accelX();
@@ -151,36 +160,41 @@ Serial.println("maxY: " + String(my));
 Serial.println("magZ: " + String(mz));
 Serial.println("horizontal direction: " + String(m_hori));
  
-Serial.println("at " + String(millis()) + "ms");
-delay(100);
+//Serial.println("at " + String(millis()) + "ms");
 
 //SDCardに出力
 // 書き込みモードでファイルを開く
   fo = SD.open(fname, FILE_APPEND);
   // 書き込む
+  if(fo){
   fo.println(String("ax: ")+ax+String("| ay: ")+ay+String("| az: ")+az+String("| a_sqrt:")+a_sqrt);
   fo.println(String("gx: ")+gx+String("| gy: ")+gy+String("| gz: ")+gz);
   fo.println(String("mx: ")+mx+String("| my: ")+my+String("| mz: ")+mz+("| m_hori:")+m_hori);
   fo.println(String("temp: ")+temperature+String("| pressure: ")+pressure+String("| alti: ")+alti);
   //fo.println(String("temp_hybrid: ")+temp);
-  Serial.println("Writing now");
+//  Serial.println("Writing now");
     // ファイルを閉じる
   fo.close();
-  delay(900);  
-
+  }
+  else{
+//    Serial.println("Error");
+  }
+ return ax;
 }
 
 //フライトピンが外れてから時間を計測してくれる関数
-void flight_timer(){
-  //フライトピンからの電圧の読み込み
+int flight_timer(){
+  
+ //フライトピンからの電圧の読み込み
  flightState = digitalRead(flightPin);
- 
+
  //フライトピン解除もしくは気圧センサーが0になったときにカウントが始まる
  if (flightState == LOW ){
+  int ax = data();
   Serial.println("LOW");
   
   //フライトピンが外されてから何週目のプログラムかを表示
-  Serial.println(i);
+  // Serial.println(i);
 
   //フライトピンが外れた時間:time_mを取得
   if(i == 0){
@@ -189,38 +203,53 @@ void flight_timer(){
 
   //フライトピンが外れた時刻の時間を計測
   //delay()は休止期間、すべての動作を止めてしまうので却下
-  unsigned long time_dif = millis() - time_m;
-
-  //time_m:フライトピンが外された時刻を表示
-  //Serial.println(String("Time :  ")+time_m);
+  unsigned int time_dif = millis() - time_m;
 
   //time_dif:フライトピンが外されてからの時間を表示
   Serial.println(String("Time :  ")+time_dif);
   
   //フライトピンが外れた時間(i=0のとき)以外、変数time_mは更新されない
   i = i+1;
-  //8秒後~10秒後にかけてモーターは動作
-  if(time_dif <= 10000 && time_dif >= 8000){
-    Serial.println("start event!"); 
-    Serial.println(String("Time :  ") + time_dif);
-    
-    //flight()関数を呼び出し
-   //flight_motor();
-  }
-  else{
-    //10秒後~ではモータは静止
-    myservo.write(motor_static); 
-    Serial.println("After behavior");
+
+    if(ax > 0){
+      data_count(ax,time_dif);
+    }
+    else{
+      motor_unactive();
+      Serial.println("unactive mode ...");
+      n = 0;
+    } 
   }
 }
 
- //動作しないときモータには85(サーボモータ固有の回転停止する値)を与える
- //サーボモータによって値は変わる
-else{
-  myservo.write(motor_static); 
-  Serial.println(flightState);
-  Serial.println("stop mode...");
-}
+
+void data_count(float ax,int time_dif){
+  n ++;
+   //open_time ms以上だと・・・
+   if(time_dif >= open_time){
+    //加速度データが正の値を10回取り続けると・・・
+    if(n > 10){
+      Serial.println("start event!"); 
+      Serial.println(String("Time :  ") + time_dif);
+      //flight()関数を呼び出し
+      flight_motor();
+     }
+     //10回未満だと
+     else{
+      motor_unactive();
+     }
+   }
+  //limit_time ms以上だと・・・
+  else if(time_dif > limit_time){
+      Serial.println("start event!"); 
+      Serial.println(String("Time :  ") + time_dif);
+      //flight()関数を呼び出し
+      flight_motor();
+  }
+  else{
+    motor_unactive();
+    Serial.println("After behavior");
+  }
 }
 
 void setup() {
@@ -228,7 +257,6 @@ while(!Serial);
 
 //シリアル通信の開始
 Serial.begin(9600);
-Serial.println("started");
 
 MPU_setup();
 BMP_setup();
@@ -238,8 +266,8 @@ BMP_setup();
   pinMode(flightPin,INPUT);
   
  //モーター接続準備
- myservo.attach(motorPin);
- pinMode(motorPin,OUTPUT);
+  myservo.attach(motorPin);
+  pinMode(motorPin,OUTPUT);
   
 if(SD.begin(cs_SD, SPI, 24000000, "/sd")){
     Serial.println("OK!");
@@ -251,7 +279,5 @@ else{
 }
 
 void loop() {
-  data();
   flight_timer();
-  delay(500);
 }
